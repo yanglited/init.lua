a0040bb972d837fb21c8f86e2328f3bdfd0f572d
Yang Li <yanglited@gmail.com>
2023-06-29 11:04:15 -0400

    added python black plugin for formating

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -8,6 +8,7 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use 'lewis6991/gitsigns.nvim'
+  use 'averms/black-nvim'
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  -- or                            , branch = '0.1.x',
